a5638f1ccdfbfd0600fed04fe1d60a00efdb4014
Dan Payne <dpayne5532@gmail.com>
2022-02-16 17:53:10 -0700

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Dan Payne on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
